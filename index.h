5097fba58f626f1a1918782961ec2a19a3afe7d8
alexander <alexmunozdiaz21@gmail.com>
2024-10-04 03:16:56 -0700

    Final p2

---
+++ index.h
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang=""en>
+<head>
+    <meta charset="UTF-8>
+    <title>first web.</p>
+</head>
+<body>
+    <h1>hi, this is my first website using htnl</h1>
+    <p>these are my firsts paragraphs in html</p>
+    <p>second one </p>
+    <p>third one</p>
+</body>
+</html>
+
